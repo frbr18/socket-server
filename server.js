1a32683e0662b8f80d6b454bad08029abd7f6d2a
Fredrik Bredenius <Bubban90@hotmail.com>
2019-10-15 17:59:38 +0100

    First version

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+let users = [];
+let messages = [];
+let index = 0;
+
+io.on('connection', socket => {
+    socket.emit('loggedIn', {
+        users: users.map(user => user.username),
+        messages: messages
+    });
+
+    socket.on('newuser', username => {
+        console.log(`${username} har anslutit.`);
+        socket.username = username;
+        users.push(socket);
+        io.emit('userOnline', socket.username);
+    });
+
+    socket.on('msg', msg => {
+        let message = {
+            index: index,
+            username: socket.username,
+            msg: msg
+        }
+
+        messages.push(message);
+
+        io.emit('msg', message);
+        index++;
+    });
+
+    //Disconnect
+    socket.on("disconnet", () => {
+        console.log(`${socket.username} har lämnat chatten.`);
+        io.emit("userLeft", socket.username);
+        users.splice(users.indexOf(socket), 1);
+    });
+});
+
+http.listen(3000, () => {
+    console.log("Server listening on port 3000");
+});
