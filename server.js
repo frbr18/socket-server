9944039190cae6041caea8566ce45e6d5cac0677
Fredrik Bredenius <Bubban90@hotmail.com>
2019-10-31 12:08:41 +0100

    done with course

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
-app.options('*', cors());
-app.disable('x-powered-by');
+
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
@@ -13,14 +11,16 @@ const bodyParser = require("body-parser");
 const msgApi = require("./routes/msg-api");
 
 let users = [];
-
 let index = 0;
 
+app.use(cors());
+app.options('*', cors());
+app.disable('x-powered-by');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/msg', msgApi);
-//io.origins(['https://me-client.frbr18-jsramverk.me:443/']);
+io.origins(['https://me-client.frbr18-jsramverk.me:443/']);
 
 io.on('connection', socket => {
     let messages = [];
@@ -58,6 +58,6 @@ io.on('connection', socket => {
     });
 });
 
-http.listen(8334, () => {
+http.listen(8335, () => {
     console.log("Server listening on port 8334");
 });
